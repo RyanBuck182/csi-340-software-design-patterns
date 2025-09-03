9c01c38efd5ee4b6b91080b3b129864f435fa465
ryanbuck182 <ryanbuck182@gmail.com>
2025-09-03 11:31:20 -0400

    Add Course class

---
+++ StudentEnrollmentSystem/src/Course.java
@@ -0,0 +1,25 @@
+public class Course {
+    private String name;
+    private String number;
+
+    public Course(String name, String number) {
+        this.name = name;
+        this.number = number;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+}
