f8ea432aa5994d425eaa96e23eaee8d52e35284b
ryanbuck182 <ryanbuck182@gmail.com>
2025-09-17 17:47:52 -0400

    Add Item and Auctioneer classes

---
+++ 4-1-Auction-ObserverPattern/src/main/auction/Auctioneer.java
@@ -0,0 +1,57 @@
+package main.auction;
+
+import main.Publisher;
+import main.Subscriber;
+import main.auction.bid.Bidder;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Auctioneer implements Publisher {
+    protected Item item;
+    protected int remainingBids;
+    protected double highestBid;
+    protected Bidder highestBidder;
+    protected List<Subscriber> subscribers = new ArrayList<Subscriber>();
+
+    public Auctioneer(Item item, int bidLimit) {
+        this.item = item;
+        this.highestBid = item.value;
+        this.remainingBids = bidLimit;
+    }
+
+    public Item getItem() {
+        return item;
+    }
+
+    public int getRemainingBids() {
+        return remainingBids;
+    }
+
+    public void addBid(Bidder bidder, double bidAmount) {
+        boolean isValidBidAmount = bidAmount > 0;
+        boolean hasBidsLeft = remainingBids > 0;
+        boolean isHighestBidder = highestBidder == bidder;
+
+        if (isValidBidAmount && hasBidsLeft && !isHighestBidder) {
+            this.highestBidder = bidder;
+            this.highestBid += bidAmount;
+            this.remainingBids--;
+            notifySubscribers();
+        }
+    }
+
+    public void addSubscriber(Subscriber subscriber) {
+        subscribers.add(subscriber);
+    }
+
+    public void removeSubscriber(Subscriber subscriber) {
+        subscribers.remove(subscriber);
+    }
+
+    public void notifySubscribers() {
+        for (Subscriber subscriber : subscribers) {
+            subscriber.update(highestBid);
+        }
+    }
+}
---
+++ 4-1-Auction-ObserverPattern/src/main/auction/Item.java
@@ -0,0 +1,11 @@
+package main.auction;
+
+public class Item {
+    public double value;
+    public String name;
+
+    public Item (double value, String name) {
+        this.value = value;
+        this.name = name;
+    }
+}
