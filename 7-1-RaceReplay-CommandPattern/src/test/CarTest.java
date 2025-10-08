d44816587165e4786a8dccdbb87293aadf296c8a
ryanbuck182 <ryanbuck182@gmail.com>
2025-10-08 11:52:50 -0400

    Add test class

---
+++ 7-1-RaceReplay-CommandPattern/src/test/CarTest.java
@@ -0,0 +1,26 @@
+package test;
+
+import main.Car;
+import main.CarGame;
+import main.command.*;
+
+public class CarTest {
+
+    public static void main(String[] args) {
+        CarGame carGame = new CarGame();
+        Car car = new Car();
+
+        TurnLeftCommand turnLeft = new TurnLeftCommand(car);
+        TurnRightCommand turnRight = new TurnRightCommand(car);
+        SpeedUpCommand speedUp = new SpeedUpCommand(car);
+        SlowDownCommand slowDown = new SlowDownCommand(car);
+
+        carGame.performRecordCommand(turnLeft);
+        carGame.performRecordCommand(speedUp);
+        carGame.performRecordCommand(slowDown);
+        carGame.performRecordCommand(turnRight);
+
+        System.out.println("REPLAY");
+        carGame.replay();
+    }
+}
