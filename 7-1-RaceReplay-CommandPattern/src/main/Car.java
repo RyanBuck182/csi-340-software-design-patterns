3b5a530eb815f59c1b2d3457702a3cf30df2aafc
ryanbuck182 <ryanbuck182@gmail.com>
2025-10-08 11:51:52 -0400

    Add car class

---
+++ 7-1-RaceReplay-CommandPattern/src/main/Car.java
@@ -0,0 +1,19 @@
+package main;
+
+public class Car {
+    public void speedUp() {
+        System.out.println("The car is speeding up!");
+    }
+
+    public void slowDown() {
+        System.out.println("The car is slowing down!");
+    }
+
+    public void turnLeft() {
+        System.out.println("The car is turning left!");
+    }
+
+    public void turnRight() {
+        System.out.println("The car is turning right!");
+    }
+}
