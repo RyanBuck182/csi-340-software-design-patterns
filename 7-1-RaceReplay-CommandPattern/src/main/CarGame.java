a147de8e7a7d439ecbc41ceae59b06d848a8271e
ryanbuck182 <ryanbuck182@gmail.com>
2025-10-08 11:52:30 -0400

    Add CarGame

---
+++ 7-1-RaceReplay-CommandPattern/src/main/CarGame.java
@@ -0,0 +1,25 @@
+package main;
+
+import main.command.Command;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class CarGame {
+    List<Command> commandHistory;
+
+    public CarGame() {
+        commandHistory = new LinkedList<>();
+    }
+
+    public void performRecordCommand(Command command) {
+        commandHistory.add(command);
+        command.execute();
+    }
+
+    public void replay() {
+        for (Command command : commandHistory) {
+            command.execute();
+        }
+    }
+}
