c20c8908a32a521afceba78aaa0fc22c56fcc378
ryanbuck182 <ryanbuck182@gmail.com>
2025-10-15 10:42:05 -0400

    Add draw library

---
+++ 8-1-GeometryDrawing-AdapterPattern/src/main/drawlib/Rectangle.java
@@ -0,0 +1,8 @@
+// Ryan Buck
+package main.drawlib;
+
+public class Rectangle {
+    public void produce(int x1, int y1, int width, int height) {
+        System.out.println(String.format("Rectangle with coordinate left-down point (%d;%d), width: %d, height: %d",  x1, y1, width, height));
+    }
+}
